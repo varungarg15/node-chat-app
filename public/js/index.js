9b60e3f829ea0bd75b4aeae4fe847061c9f97580
Varun Garg <gargvarun15031998@gmail.com>
2018-11-01 13:48:26 +0530

    Emmit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,29 +2,18 @@ var socket = io();
 
     socket.on('connect', function() {
         console.log('Connected to Server');
-        
-    //   socket.emit('createEmail', {
-    //       to: 'vipul@examlple.com',
-    //       text: 'Hey. This is varun'
-    //   });
- 
-    //1
-      socket.emit('createMessage', {
-          from: 'Varun',
-          text: 'Class ka time ho raha h'
-      });
     });
+    //1
+    //   socket.emit('createMessage', {
+    //       from: 'Varun',
+    //       text: 'Class ka time ho raha h'
+    //   });
+    // });
 
     socket.on('disconnect', function() {
         console.log('Disconnected from server');
     });
-
-
-    // socket.on('newEmail', function (email) {
-    //     console.log('New Email', email);
         
-    // });
-     
     //2
     socket.on('newMessage', function (message) {
         console.log('nMessage', message);
--- server/server.js
+++ server/server.js
@@ -13,29 +13,24 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
     console.log('New User Connected');
-    
-
-    // socket.emit('newEmail', {
-    //     from: 'varun@example.com',
-    //     text: 'Hey. What is going on.',
-    //     createdAt: 123
-    // });
-    
+   
     //2
-    socket.emit('newMessage', {
-        from: 'Vipul',
-        text: 'What are you doing',
-        createdAt: 456
-    });
+    // socket.emit('newMessage', {
+    //     from: 'Vipul',
+    //     text: 'What are you doing',
+    //     createdAt: 456
+    // });
 
-    //  socket.on('createEmail', (newEmail) => {
-    //      console.log('create-email', newEmail);
-         
-    //  });
-
+   
      //1
      socket.on('createMessage', (message) => {
          console.log('create-message', message);
+
+         io.emit('newMessage', {
+           from: message.from,
+           text: message.text,
+           createdAt: new Date().getTime()
+         });
          
      });
 
