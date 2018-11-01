f937d2b31f38f29503d8ca4e960ca9fdb35b45ee
Varun Garg <gargvarun15031998@gmail.com>
2018-11-01 21:52:54 +0530

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -35,11 +35,12 @@ var socket = io();
     jQuery('#message-form').on('submit', function (e) {
         e.preventDefault();
 
+        var messageTextbox = jQuery('[name=message]');
         socket.emit('createMessage', {
             from: 'User',
             text: jQuery('[name=message]').val()
         }, function () {
-
+         messageTextbox.val('');
         });
     });
 
@@ -49,13 +50,17 @@ var socket = io();
             return alert('Geolocation is not supported by your browser');
         }
 
+        locationButton.attr('disabled', 'disabled').text('Sending location ...');
+
         navigator.geolocation.getCurrentPosition(function (position) {
+            locationButton.removeAttr('disabled').text('Send location');
            socket.emit('createLocationMessage', {
                latitude: position.coords.latitude,
                longitude: position.coords.longitude
            });
             
         }, function () {
+            locationButton.removeAttr('disabled').text('Send location');
             alert('Unable to fetch location');
         });
     });
--- server/server.js
+++ server/server.js
@@ -30,7 +30,7 @@ io.on('connection', (socket) => {
          console.log('create-message', message);
 
          io.emit('newMessage', generateMessage(message.from, message.text));
-         callback('This is from Server');
+         callback();
         // socket.broadcast.emit('newMessage', {
         //       from: message.from,
         //       text: message.text,
