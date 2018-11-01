fad381675c55baca692bc450d9f9a4008518eaae
Varun Garg <gargvarun15031998@gmail.com>
2018-11-01 16:27:47 +0530

    create generateMessage utility

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
+const {generateMessage} = require('./utils/message');
 const publicPath = path.join(__dirname , '../public');
 const port = process.env.PORT || 3000;
 var app = express();
@@ -21,26 +22,14 @@ io.on('connection', (socket) => {
     //     createdAt: 456
     // });
 
-     socket.emit('newMessage', {
-         from: 'Admin',
-         text: 'Welcome to the chat app',
-         createdAt: new Date().getTime()
-     });
+     socket.emit('newMessage', generateMessage('admin', 'Welcome to the chat app'));
 
-     socket.broadcast.emit('newMessage', {
-         from: 'Admin',
-         text: 'New user joined the chat room',
-         createdAt: new Date().getTime()
-     });
+     socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
      //1
      socket.on('createMessage', (message) => {
          console.log('create-message', message);
 
-         io.emit('newMessage', {
-           from: message.from,
-           text: message.text,
-           createdAt: new Date().getTime()
-         });
+         io.emit('newMessage', generateMessage(message.from, message.text));
          
         // socket.broadcast.emit('newMessage', {
         //       from: message.from,
