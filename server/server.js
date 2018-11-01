0893ef5ef551de4913e272e8e9b5b0ec4f850adc
Varun Garg <gargvarun15031998@gmail.com>
2018-11-01 13:34:55 +0530

    Add newMessage and createMessage events

--- server/server.js
+++ server/server.js
@@ -15,6 +15,31 @@ io.on('connection', (socket) => {
     console.log('New User Connected');
     
 
+    // socket.emit('newEmail', {
+    //     from: 'varun@example.com',
+    //     text: 'Hey. What is going on.',
+    //     createdAt: 123
+    // });
+    
+    //2
+    socket.emit('newMessage', {
+        from: 'Vipul',
+        text: 'What are you doing',
+        createdAt: 456
+    });
+
+    //  socket.on('createEmail', (newEmail) => {
+    //      console.log('create-email', newEmail);
+         
+    //  });
+
+     //1
+     socket.on('createMessage', (message) => {
+         console.log('create-message', message);
+         
+     });
+
+
     socket.on('disconnect', () => {
         console.log('User was disconnected');
         
